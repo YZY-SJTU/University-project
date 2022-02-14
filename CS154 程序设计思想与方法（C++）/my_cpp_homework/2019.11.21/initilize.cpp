8f5e00f977610e61e7fb374b99e0539d32b8d726
spoilvoid <1651717534@qq.com>
2022-02-14 21:03:27 +0800

    delete redundant code

--- CS154 程序设计思想与方法（C++）/my_cpp_homework/2019.11.21/initilize.cpp
+++
@@ -1,8 +0,0 @@
-#include"12_2.h"
-
-extern StuInfo *head,*rear;
-
-void initilize()
-{
-    head=rear=new StuInfo;
-}
--- CS154 程序设计思想与方法（C++）/my_cpp_homework/test and question/test3.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cstring>
-using namespace std;
-
-int main()
-{   char n[]={'a','b','\0'};
-    cout <<n[0]+1;
-
-}
